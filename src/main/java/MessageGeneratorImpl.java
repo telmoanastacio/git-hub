c635ec9d9df592bd9aff4892f90cce649798be15
Telmo Silva <telmo.anastacio@gmail.com>
2019-02-21 10:07:20 +0000

    Added MessageGenerator interface

---
+++ src/main/java/MessageGeneratorImpl.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class MessageGeneratorImpl implements MessageGenerator
+{
+    // == variables ==
+    private String email;
+    private List<String> allRepositories;
+    private Map<String, Double> overallLangStats;
+
+    // == constructors ==
+    public MessageGeneratorImpl(String email, List<String> allRepositories, HashMap<String, Double> overallLangStats)
+    {
+        this.email = email;
+        this.allRepositories = allRepositories;
+        this.overallLangStats = overallLangStats;
+    }
+
+    // == public methods ==
+    public String generateStatsMessage()
+    {
+        StringBuilder sb = new StringBuilder();
+        Set<String> overallLangStatsKeys = this.overallLangStats.keySet();
+
+        sb.append("=============================\n");
+        sb.append("== STATS MESSAGE GENERATED ==\n");
+        sb.append("=============================\n\n");
+
+        sb.append("=> USER EMAIL: " + this.email + "\n\n");
+
+        sb.append("=> REPOSITORIES:\n");
+        for(String repository : this.allRepositories)
+        {
+            sb.append("- " + repository + "\n");
+        }
+        sb.append("\n");
+
+        sb.append("=> GLOBAL LANGUAGE STATISTICS:\n");
+        for(String key : overallLangStatsKeys)
+        {
+            sb.append("- " + key + ": " + this.overallLangStats.get(key) + "%\n");
+        }
+
+        sb.append("=============================\n");
+        sb.append("=============================\n");
+        return sb.toString();
+    }
+}
