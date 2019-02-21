c327d4df4e29803626920fb0dd900597c41a8d74
Telmo Silva <telmo.anastacio@gmail.com>
2019-02-21 19:30:46 +0000

    dependencies added

--- src/main/java/com/tsilva/github/MessageProcessingImpl.java
+++ src/main/java/com/tsilva/github/MessageProcessingImpl.java
@@ -1,11 +1,25 @@
 package com.tsilva.github;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MessageProcessingImpl
 {
     private final String JSON_STRING;
+    private JSONObject jsonObject;
 
     public MessageProcessingImpl(String jsonString)
     {
         this.JSON_STRING = jsonString;
+        try
+        {
+            this.jsonObject = new JSONObject(jsonString);
+            //TODO: collect the info from JSON files
+        }
+        catch(JSONException e)
+        {
+            e.printStackTrace();
+            System.out.println("== parsing exception ==");
+        }
     }
 }
