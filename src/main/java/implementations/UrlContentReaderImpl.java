a18b76701c855344fba965d958fc10f87079299b
Telmo Silva <telmo.anastacio@gmail.com>
2019-02-21 17:49:35 +0000

    switched connetion method to http

--- src/main/java/implementations/UrlContentReaderImpl.java
+++ src/main/java/implementations/UrlContentReaderImpl.java
@@ -1,11 +1,10 @@
 package implementations;
 
 import interfaces.UrlContentReader;
-import sun.misc.IOUtils;
 
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class UrlContentReaderImpl implements UrlContentReader
 {
@@ -19,7 +18,7 @@ public class UrlContentReaderImpl implements UrlContentReader
         try
         {
             URL url = new URL(urlString);
-            URLConnection con = url.openConnection();
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
             InputStream in = con.getInputStream();
             String encoding = con.getContentEncoding();  // ** WRONG: should use "con.getContentType()" instead but it returns something like "text/html; charset=UTF-8" so this value must be parsed to extract the actual encoding
             encoding = encoding == null ? "UTF-8" : encoding;
