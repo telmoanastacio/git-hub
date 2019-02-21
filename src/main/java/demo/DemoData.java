1c81dfa2879678e37efc53800e67b46829ee0146
Telmo Silva <telmo.anastacio@gmail.com>
2019-02-21 10:48:48 +0000

    added demo data

---
+++ src/main/java/demo/DemoData.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class DemoData
+{
+    public static final String USER = "telmoanastacio";
+    public static final String OWNER = USER;
+    public static final String REPO = "starter-web";
+    public static final String FILE = "README.md";
+}
