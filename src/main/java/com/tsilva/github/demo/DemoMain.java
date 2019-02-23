3dcf8c83d85bfeae051d1906f21cb12db44c400b
Telmo Silva <telmo.anastacio@gmail.com>
2019-02-23 10:25:53 +0000

    minor testing

--- src/main/java/com/tsilva/github/demo/DemoMain.java
+++ src/main/java/com/tsilva/github/demo/DemoMain.java
@@ -2,44 +2,29 @@ package com.tsilva.github.demo;
 
 import com.tsilva.github.*;
 
-import java.util.Map;
-
 public class DemoMain
 {
     public static void main(String[] args)
     {
+        // ========= To delete =========
         UrlPathGenerator urlPathGenerator = new UrlPathGeneratorImpl();
-
-        UrlContentReader userContent = new UrlContentReaderImpl();
-        UrlContentReader userReposContent = new UrlContentReaderImpl();
-        UrlContentReader repoContent = new UrlContentReaderImpl();
-        UrlContentReader fileContent = new UrlContentReaderImpl();
-
-        MessageProcessingModule2 globalStats = new MessageProcessingModule2();
 
         System.out.println(urlPathGenerator.getUserInfoUrl(DemoData.USER));
         System.out.println(urlPathGenerator.getUserRepositoryListUrl(DemoData.USER));
         System.out.println(urlPathGenerator.getRepositoryLanguageStatisticsUrl(DemoData.OWNER, DemoData.REPO));
         System.out.println(urlPathGenerator.getRepositoryContentListURL(DemoData.OWNER, DemoData.REPO));
         System.out.println(urlPathGenerator.getRepositoryFileContentURL(DemoData.OWNER, DemoData.REPO, DemoData.FILE));
+        // =============================
 
-//        // print user content
-//        System.out.println(userContent.urlRead(urlPathGenerator.getUserInfoUrl(DemoData.USER)));
-//        userContent = null;
-//        // print file content
-//        System.out.println(fileContent
-//                .urlRead(urlPathGenerator.getRepositoryFileContentURL(DemoData.OWNER, DemoData.REPO, DemoData.FILE)));
+        MessageProcessingService messageProcessingService = new MessageProcessingServiceImpl();
 
-//        String userResult = userContent.urlRead(urlPathGenerator.getUserInfoUrl(DemoData.USER));
-//        System.out.println(new MessageProcessingModule1(userResult).getEmail());
-//
-//        String userRepos = userReposContent.urlRead(urlPathGenerator.getUserRepositoryListUrl(DemoData.USER));
-//        System.out.println(new MessageProcessingModule1(userRepos).getRepos());
-//
-//        String repoResult = repoContent.urlRead(urlPathGenerator.getRepositoryLanguageStatisticsUrl(DemoData.OWNER, DemoData.REPO));
-//        System.out.println(new MessageProcessingModule1(repoResult).getStatistics());
+        System.out.println("== email ==");
+        System.out.println(messageProcessingService.getEmail());
 
-        Map<String, Integer> globalStatsResult = globalStats.getGlobalStatistics();
-        System.out.println(globalStatsResult);
+        System.out.println("== repos ==");
+        System.out.println(messageProcessingService.getRepos());
+
+        System.out.println("== global stats ==");
+        System.out.println(messageProcessingService.getGlobalStatistics());
     }
 }
