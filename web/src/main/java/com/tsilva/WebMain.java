7a56cb7668e4317745ed49473a6c3bdb2ea0b794
Telmo Silva <telmo.anastacio@gmail.com>
2019-02-26 13:41:31 +0000

    application link generated

--- web/src/main/java/com/tsilva/WebMain.java
+++ web/src/main/java/com/tsilva/WebMain.java
@@ -1,11 +1,30 @@
 package com.tsilva;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class WebMain
+public class WebMain implements CommandLineRunner
 {
+    @Autowired
+    Environment env;
+
+    @Override
+    public void run(String... args) throws Exception
+    {
+        System.out.println("======================");
+        System.out.println("== APPLICATION LINK ==");
+        System.out.println("======================");
+        String port = env.getProperty("local.server.port");
+        System.out.println("http://localhost:" + port + "/");
+        System.out.println("== OR ==");
+        System.out.println("http://127.0.0.1:" + port + "/");
+        System.out.println("======================");
+    }
+
     public static void main(String[] args)
     {
         SpringApplication.run(WebMain.class, args);
